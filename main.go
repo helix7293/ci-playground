1604226d5c1e57073267ca32abb3d74efe69a8a7
Johana Foster <johana.chiakifoster@99designs.com>
2016-07-13 13:37:59 +1000

    Log line

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ func main() {
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
-
+	log.Print("Listening on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
