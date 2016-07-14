bdaa176ca240b0dd1d92335de0b49dfdbc2041ec
Johana Foster <johana.chiakifoster@99designs.com>
2016-07-14 17:20:45 +1000

    Deploy script

---
+++ .buildkite/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+
+buildkite-agent artifact download ci-playground .
+sudo /opt/ci-playground/update.sh ci-playground
