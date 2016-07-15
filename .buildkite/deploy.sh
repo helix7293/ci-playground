58c87476eba9ee8aa583479f0493a1fe57d033a2
Johana Foster <johana.chiakifoster@99designs.com>
2016-07-15 11:02:28 +1000

    Temp dir

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -5,5 +5,6 @@ set -euxo pipefail
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME/go
 
-buildkite-agent artifact download ci-playground ./
-sudo /opt/ci-playground/update.sh ci-playground
+mkdir -p tmp 
+buildkite-agent artifact download ci-playground tmp/
+sudo /opt/ci-playground/update.sh tmp/ci-playground
